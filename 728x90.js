feaf420b100a5b8f3dbf610998959e5aa8344f88
kaylamyid59-dev <kaylamyid59@gmail.com>
2025-12-28 18:59:53 +0800

    Create 728x90.js
    banner adstera 728x90

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'fe231bbdf46d275fea9c1699e4d5568e',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="https://www.highperformanceformat.com/fe231bbdf46d275fea9c1699e4d5568e/invoke.js"></scr' + 'ipt>');
