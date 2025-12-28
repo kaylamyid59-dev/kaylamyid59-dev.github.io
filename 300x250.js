27c3d089fc0bf36ae6180fda918846897a703188
kaylamyid59-dev <kaylamyid59@gmail.com>
2025-12-28 19:01:08 +0800

    Create 300x250.js
    banner adstera 300x250

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '58fb908ad152fd37772ee21ec8810c36',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="https://www.highperformanceformat.com/58fb908ad152fd37772ee21ec8810c36/invoke.js"></scr' + 'ipt>');
